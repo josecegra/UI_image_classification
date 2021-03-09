b6f67cb72c416559a77d6a818e26c4f9a55d1a63
jcejudo <joseed.cejudo@europeana.eu>
2021-03-09 21:23:29 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# UI_image_classification
+# UI Image Classification
+
+pip install -r requirements.txt
+
+python src/manage.py runserver 0:8001
+
+http://{domainName}:8001/datasets
---
+++ src/static/js/main.js
@@ -0,0 +1,7 @@
+let options = {
+    width: 400,
+    height: 200,
+    zoomWidth: 400,
+    offset: {vertical: 10, horizontal: 10}
+  };
+new ImageZoom(document.getElementById("img-container"), options);
